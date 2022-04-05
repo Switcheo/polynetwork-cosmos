2b19317126329bf3a67964f370a7b404450c0a4c
holyxiaoxin <jr.loves.maths@gmail.com>
2022-04-05 16:20:08 +0800

    feat: update proto package to match carbon

--- x/btcx/keeper/keeper_test.go
+++ x/btcx/keeper/keeper_test.go
@@ -43,7 +43,7 @@ func createTestApp(isCheckTx bool) (*simapp.SimApp, sdk.Context, types.QueryClie
 	queryClient := types.NewQueryClient(queryHelper)
 	return app, ctx, queryClient
 }
-
+ererkeep
 func btcx_initSupply(t *testing.T, app *simapp.SimApp, ctx sdk.Context) sdk.Coins {
 	coinsStr := "1000000000btca,1000000000000000000btcb"
 	initTokens, err := sdk.ParseCoinsNormalized(coinsStr)
