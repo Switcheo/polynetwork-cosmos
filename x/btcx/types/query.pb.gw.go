a73f90fa6c4399392c769b47a1d4a5067360c51f
yan-soon <soonxiangyan@gmail.com>
2024-01-24 19:32:26 +0800

    fix bug with proto grpc query

--- x/btcx/types/query.pb.gw.go
+++ x/btcx/types/query.pb.gw.go
@@ -300,9 +300,9 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_DenomInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"Switcheo", "polynetworkcosmos", "btcx", "DenomInfo", "denom"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_DenomInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"cosmospoly", "btcx", "DenomInfo", "denom"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_DenomCrossChainInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"Switcheo", "polynetworkcosmos", "btcx", "DenomInfo", "denom", "chain_id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_DenomCrossChainInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 1, 0, 4, 1, 5, 4}, []string{"cosmospoly", "btcx", "DenomInfo", "denom", "chain_id"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
